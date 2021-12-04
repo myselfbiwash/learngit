2f0479a8553056773de81bc22bd0f898e9f1a35b
Biwash Budhathoki <biwashbudhathoki100@gmail.com>
2021-12-04 18:00:49 +0545

    diff(32)

--- diff.py
+++ diff.py
@@ -1,8 +1,9 @@
 def difference(n):
-    if n <= 17:
+    if n <= 17:gi
         return 17 - n
     else:
         return (n - 17) * 2 
 
 print(difference(22))
+print(difference(32))
 print(difference(14))
